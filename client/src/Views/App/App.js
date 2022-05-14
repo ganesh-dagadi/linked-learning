3bb139cbfaf8c1b93f6b5c5f0cf782c273b2809e
Ganesh D <ganeshdagadi3@gmail.com>
2022-05-14 18:51:13 +0530

    Dashboard till search

--- client/src/Views/App/App.js
+++ client/src/Views/App/App.js
@@ -6,24 +6,38 @@ import { Routes, Route, Link } from "react-router-dom"
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/dashboard' element={<DashBoard />} />
-      </Routes>
+      <div className='container'>
+        <div className='header'>
+          <span className='logo'><h3>Linked Learning</h3></span>
+          <button className='donate btn-success'>Donate</button>
+        </div>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/user/:id' element={<User />} />
+          <Route path='/dashboard' element={<DashBoard />} />
+        </Routes>
 
-      <nav className="navbar">
-        <div className="navbar-link active">
-          <a href="/" id="home"><div>Home</div></a>
-        </div>
-        <div className="navbar-link">
-          <a href="/about" id="about"><div>About</div></a>
-        </div>
-        <div className="navbar-link">
-          <a href="/learn" id="learn"><div>Learn</div></a>
-        </div>
-      </nav>
+        <nav className="navbar">
+          <div className="navbar-link active">
+            <Link to="/">Home</Link>
+          </div>
+          <div className="navbar-link">
+            <Link to="/user/450505"><img src='https://randomuser.me/api/portraits/women/91.jpg' /></Link>
+          </div>
+          <div className="navbar-link">
+            <a href="/learn" id="learn"><div>Logout</div></a>
+          </div>
+        </nav>
+      </div>
     </div>
   );
 }
 
+function User(props) {
+  return (
+    <div>
+      User page
+    </div>
+  )
+}
 export default App;
--- client/src/Views/DashBoard/DashBoard.js
+++ client/src/Views/DashBoard/DashBoard.js
@@ -1,8 +1,58 @@
-
+import './DashBoard.css'
 function DashBoard(props) {
     return (
         <div className="container">
-            <h1>The Dahsboard page</h1>
+
+            <div className="search">
+                <div class=" search-bar col-12 col-md-10 col-lg-8">
+                    <form class="card card-sm">
+                        <div class="card-body row no-gutters align-items-center">
+
+                            <div class="col">
+                                <input class="form-control form-control-lg form-control-borderless" type="search" placeholder="Search topics or keywords" />
+                            </div>
+
+                            <div class="col-auto">
+                                <button class="btn btn-lg btn-success" type="submit">Search</button>
+                            </div>
+
+                        </div>
+                    </form>
+                </div>
+
+                <div className="by-category">
+                    <h1>Search by category</h1>
+                    <div className="row">
+                        <div className="col-lg-2">
+                            <button>IT/Software</button>
+                        </div>
+                        <div className="col-lg-2">
+                            <button>Music</button>
+                        </div>
+                        <div className="col-lg-2">
+                            <button>Art</button>
+                        </div>
+                        <div className="col-lg-2">
+                            <button>Career</button>
+                        </div>
+                        <div className="col-lg-2">
+                            <button>Self help</button>
+                        </div>
+                        <div className="col-lg-2">
+                            <button>Engineering</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <hr />
+
+            <div className='get-learning'>
+                <h1>Get back to learning</h1>
+            </div>
+
+
+
         </div>
     )
 }
